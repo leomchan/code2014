788ee725d376c5efe4d5c43091b558c5159cd443
Leo Chan <leo.chan@nobisstudios.com>
2014-03-02 11:44:30 -0700

    Added job to determine what is trending

--- parse/cloud/main.js
+++ parse/cloud/main.js
@@ -1,6 +1,57 @@
+Parse.Cloud.job("determineTrending", function (request, status) {
+	var query = new Parse.Query('CountryInfo');
+	query.limit(1000);
+	query.find()
+	.then(function (infos) {
+		// Get trends from google
+		return Parse.Cloud.httpRequest({
+			url: "http://www.google.com/trends/hottrends/atom/hourly"
+		})
+		.then(function (response) {
+			var text = response.text.toLowerCase();
+			var trends = [];
+			var itemIndex = text.indexOf("<li>");
+			while (itemIndex >= 0) {
+				// Find end of item
+				var endIndex = text.indexOf("</a>", itemIndex);
+				var startIndex = text.lastIndexOf(">", endIndex) + 1;
+				trends.push(text.substring(startIndex, endIndex).trim());
+				itemIndex = text.indexOf("<li>", endIndex);
+			}
 
-// Use Parse.Cloud.define to define as many cloud functions as you want.
-// For example:
-Parse.Cloud.define("hello", function(request, response) {
-  response.success("Hello world!");
+			function checkCountryInTrends(remainingInfos) {
+				if (remainingInfos.length > 0) {
+					var info = remainingInfos[0];
+					var inTrends = false;
+					var countryName = info.get("englishName").trim().toLowerCase();
+					trends.forEach(function (trend) {
+						if (countryName.indexOf(trend) >= 0) {
+							inTrends = true;
+							return false;
+						}
+						else {
+							return true;
+						}
+					});
+
+					info.set("trending", inTrends);
+					return info.save()
+					.then(function () {
+						return checkCountryInTrends(remainingInfos.slice(1));
+					});
+				}
+				else {
+					return Parse.Promise.as(true);
+				}
+			}
+
+			return checkCountryInTrends(infos.slice());
+		});
+
+	})
+	.then(function () {
+		status.success();
+	}, function (error) {
+		status.error(error);
+	});
 });
